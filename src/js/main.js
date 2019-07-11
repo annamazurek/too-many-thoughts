b66aa3a44a413e1a43a1704e90216fccb43cf72c
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-07-11 13:34:59 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import moment from 'moment';
+
+console.log(moment().format('MM DD YYYY, h:mm:ss a'))
+
+/*
+//NAUKA ARRAY METHODS
+const mySkills = ['html', 'css', 'js', 'git', 'gulp', 'figma'];
+
+// dodawanie i usuwanie elementów z tablicy
+// pop / push - mutują naszą tablicę
+// usunięcie ostatniego elementu:
+mySkills.pop();
+console.log(mySkills);
+// dodanie ostatniego elementu:
+mySkills.push('BEM');
+console.log(mySkills);
+// shift / unshift
+// j/w, ale dotyczą pierwszego elementu:
+mySkills.shift();
+console.log(mySkills);
+mySkills.unshift('html5');
+console.log(mySkills);
+
+// ŁĄCZENIE TABLIC
+// pobranie diva o id 'box' i stworzenie elementu <p></p>
+const box = document.querySelector('.main');
+const div = document.createElement('p');
+// 2 tablice, np. ściągnięte z 2 różnych serwerów:
+const music = ['jazz', 'rock'];
+const music2 = ['blues', 'pop'];
+// połączenie tablic - [].concat() - i dodanie jeszcze kilku elementów:
+const allMusic = [].concat(music, music2, ['metal', 'indie']);
+// dodanie przecinka i spacji między elementami tablicy - metoda join():
+div.innerHTML = allMusic.join(', ');
+//dodanie stworzonego tagu jako dziecko naszego boxa:
+box.appendChild(div);
+*/
