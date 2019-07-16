b6ee7875bdd2455eab6d345a08085fbe09381f6c
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-07-16 16:58:59 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -8,20 +8,23 @@ const loadData = document.querySelector('.button__load--js');
 const saveData = document.querySelector('.button__save--js');
 const textarea = document.querySelector('.textarea--js');
 const hidePrompt = document.querySelector('.prompt');
+const form = document.querySelector('.form');
 const showQuestions = document.querySelector('.form__questions');
 
 
 saveData.addEventListener('click', (e) => {
+  e.preventDefault(); //usuwa domyślne zachowanie przeglądarki - wciśnięcie przycisku nie powoduje teraz przeładowania strony
   localStorage.setItem('textarea', textarea.value);
 });
 loadData.addEventListener('click', (e) => {
+  e.preventDefault(); 
   textarea.value = localStorage.getItem('textarea');
 });
 
 saveData.addEventListener('mouseover', (e) => {
   hidePrompt.classList.add('prompt--hidden');
 });
-textarea.addEventListener('mouseover', (e) => {
+form.addEventListener('mouseover', (e) => {
   showQuestions.classList.toggle('form__questions--show');
 });
 
