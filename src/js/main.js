5b75618ad16d1038505d3533d5ac90a49588a914
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-12-20 19:47:57 +0100

    Feat: show info about saved note

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 const loadData = document.querySelector('.button__load--js');
 const saveData = document.querySelector('.button__save--js');
 const textarea = document.querySelector('.form__textarea--js');
+// const saveInfo = document.querySelector
 
 saveData.addEventListener('click', (e) => {
   localStorage.setItem('textarea', textarea.value);
+  document.body.classList.add('button__save--showInfo');
+  window.setTimeout(() => document.body.classList.remove('button__save--showInfo'), 4900
+    );
 });
+
 loadData.addEventListener('click', (e) => {
   textarea.value = localStorage.getItem('textarea');
 });
